5f133bb71bbdc79ca2eec0d34d76eea5d1b6a6fe
Yongjun <yongjuni30@gmail.com>
2024-12-05 16:39:10 +0900

    reset

--- custom-library/src/main/java/testActuator/test/CustomHandlerInterceptor.java
+++ custom-library/src/main/java/testActuator/test/CustomHandlerInterceptor.java
@@ -27,53 +27,72 @@ public class CustomHandlerInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 특정 URL로의 요청을 처리
-        long startTime = System.currentTimeMillis();
-        requestURI = request.getRequestURI();
-        //요청 객체에 새로운 속성 부여
-        request.setAttribute("startTime", startTime);
-        request.setAttribute("OriginalUri",requestURI);
-        request.setAttribute("no error", false);
+        try {
+            request.setAttribute("error in HandlerInterceptor", false);
+            // 특정 URL로의 요청을 처리
+            long startTime = System.currentTimeMillis();
+            //request.getreqestURI가 null인지 체크
+        /*
+        if(request.getRequestURI() == null) {
+            request.setAttribute("error in HandlerInterceptor",true);
+        }
+         */
+            requestURI = request.getRequestURI();
+            //요청 객체에 새로운 속성 부여
+            request.setAttribute("startTime", startTime);
+            request.setAttribute("OriginalUri", requestURI);
+            request.setAttribute("no error", false);
+        } catch(Exception e) {
+            request.setAttribute("error in HandlerInterceptor", true);
+            e.printStackTrace();
+        }
         return true;
 
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-       //에러 발생 안했으면 true로 부여
-        request.setAttribute("no error", true);
+        try {
+            System.out.println("async thread called postHandler");
+            //에러 발생 안했으면 true로 부여
+            request.setAttribute("no error", true);
+        } catch (Exception e){
+            request.setAttribute("error in HandlerInterceptor", true);
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
-        long startTime = (Long) request.getAttribute("startTime");
-        long endTime = System.currentTimeMillis();
-        long executeTime = endTime - startTime;
-        String isError = "";
-        LocalDateTime currentTime = LocalDateTime.now();
-
-        if( (Boolean) request.getAttribute("no error")) {
-            isError = "no error";
-            System.out.println(request.getRequestURI());
-            System.out.println("no error");
-        } else {
-            isError = "error!";
-            System.out.println(request.getRequestURI());
-            System.out.println("error !");
-        }
-        if(!request.getRequestURI().equals("/error")) {
-            Long memoryUsage = analyzeMemoryUsage();
-            meterRegistry.counter("custom.requests", "uri", request.getRequestURI(), "requestNum", String.valueOf(requestNum.incrementAndGet()))
-                    .increment();
-
-            meterRegistry.gauge("custom.memory.usage", Tags.of(
-                    "requestNum", String.valueOf(requestNum),
-                    "memoryUsage", String.valueOf(memoryUsage),
-                    "uri", request.getRequestURI() + "-" + requestNum,
-                    "error", isError + "-" + requestNum,
-                    "executingTime", String.valueOf(executeTime) + "ms",
-                    "currentTime", String.valueOf(currentTime)), requestNum);
+        try {
+            System.out.println("async thread called afterCompletion");
+            Long startTime = (Long) request.getAttribute("startTime");
+            if (startTime == null) {
+            }
+            Long endTime = System.currentTimeMillis();
+            Long executeTime = endTime - startTime;
+            String isError = "";
+            LocalDateTime currentTime = LocalDateTime.now();
+            if ((Boolean) request.getAttribute("no error")) {
+                isError = "no error";
+            } else {
+                isError = "error!";
+            }
+            if (!request.getRequestURI().equals("/error")) {
+                Long memoryUsage = analyzeMemoryUsage();
+                meterRegistry.counter("custom.requests", "uri", request.getRequestURI(), "requestNum", String.valueOf(requestNum.incrementAndGet()))
+                        .increment();
+                meterRegistry.gauge("custom.memory.usage", Tags.of(
+                        "requestNum", String.valueOf(requestNum),
+                        "memoryUsage", String.valueOf(memoryUsage) + "-" + requestNum,
+                        "uri", request.getRequestURI() + "-" + requestNum,
+                        "error", isError + "-" + requestNum,
+                        "executingTime", String.valueOf(executeTime) + "ms" + "-" + requestNum,
+                        "currentTime", String.valueOf(currentTime) + "-" + requestNum), requestNum);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
 
         }
 
